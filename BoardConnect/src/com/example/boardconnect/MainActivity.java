c346eb6bdeabf97e216916976801bac49065b902
Sandesh Ghimire <sandeshghimire@outlook.com>
2014-06-29 14:20:45 -0700

    Main

--- BoardConnect/src/com/example/boardconnect/MainActivity.java
+++ BoardConnect/src/com/example/boardconnect/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
@@ -23,7 +24,7 @@ public class MainActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -49,7 +50,7 @@ public class MainActivity extends ActionBarActivity {
 	 */
 	public static class PlaceholderFragment extends Fragment {
 
-		public PlaceholderFragment() {
+		public PlaceholderFragment(){
 		}
 
 		@Override
